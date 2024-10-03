ee14eebda8c878e7115a431c10bab11eb913c617
Darius Zilionis <Darius.Zilionis@aol.com>
2024-10-03 17:10:14 +0300

    initial commit

--- reactLessonNo1/src/App.jsx
+++ reactLessonNo1/src/App.jsx
@@ -4,14 +4,16 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Header } from "./Header";
 import { Article } from "./Article";
+import CardComponent, { OtherCard } from "./CardComponent";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
-      <Article />
+      <CardComponent title="Uno" text="fun card game." />
+      <CardComponent title="basketball" text="fun card game." />
+      <OtherCard title="football" text="fun card game." />
     </>
   );
 }
